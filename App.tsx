d305fe8475198a657fe10aa9e6816b3266ba6f59
Peter Stakoun <peter@stakoun.com>
2021-10-04 19:59:56 -0400

    Initial commit

---
+++ App.tsx
@@ -0,0 +1,14 @@
+import React from "react"
+import { SafeAreaView, ScrollView, Text } from "react-native"
+
+const App = () => {
+  return (
+    <SafeAreaView>
+      <ScrollView>
+        <Text>TODO</Text>
+      </ScrollView>
+    </SafeAreaView>
+  )
+}
+
+export default App
